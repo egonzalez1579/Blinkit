e377d84017e06472f94e95b27eff021e59ea5d57
Edwin Gonzalez <gonz0202@algonquinlive.com>
2025-02-28 12:31:47 -0500

    Create API for User Details Update

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { registerUserController } from '../controllers/user.controller.js';
-import { verifyEmailController } from '../controllers/user.controller.js';
-import { loginController } from '../controllers/user.controller.js';
-import { logoutController } from '../controllers/user.controller.js';
+import {
+  registerUserController,
+  updateUserDetails,
+  verifyEmailController,
+  loginController,
+  logoutController,
+  uploadAvatar,
+} from '../controllers/user.controller.js';
+
 import auth from '../middleware/auth.js';
-import { uploadAvatar } from '../controllers/user.controller.js';
 import upload from '../middleware/multer.js';
 
 const userRouter = Router();
@@ -14,6 +18,7 @@ userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginController);
 userRouter.get('/logout', auth, logoutController);
 userRouter.put('/upload-avatar', auth, upload.single('avatar'), uploadAvatar);
+userRouter.put('/update-user', auth, updateUserDetails);
 
 export default userRouter;
 // The user.route.js file contains the userRouter object, which is an instance of the Express Router. The userRouter object is used to define the routes for user-related operations in the application.
